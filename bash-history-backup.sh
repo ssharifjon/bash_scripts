f7282f24be11b56905cf292aa8822767ac4ab9c0
Sharifjon Sharipov <ss2393506@gmail.com>
2026-03-02 17:27:46 +0000

    Add files via upload

---
+++ bash-history-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# creates a backup of ~/.bash_history 
+# places it in ~/history_backups/ directory
+# names it after the date that the commands were executed on
+# in YYMMDD format
+#
+# SETUP:
+#	$mkdir -p ~/history_backups
+#	$date +"%y%m%d" > ~/history_backups/naming
+#	and, add the path to this script to your .bash_profile, .profile, etc.
+	
+
+HISTORY_FILE="$HOME/.bash_history"
+BACKUP_DIR="$HOME/history_backups"
+log_file="$BACKUP_DIR/naming"
+previous_date="$(<$log_file)"
+current_date=$(date +"%y%m%d")
+
+if [ $previous_date != $current_date ]; then		
+	cp $HISTORY_FILE $BACKUP_DIR/$previous_date	
+	truncate -s 0 $HISTORY_FILE			
+	echo $current_date > $log_file			
+fi
+
+
