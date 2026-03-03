df4769690fa9ab3eb7a885d5b2c6bd54f69295cf
Sharifjon Sharipov <ss2393506@gmail.com>
2026-03-03 20:47:42 +0000

    include du feature in daily.sh

---
+++ daily.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# creates a backup of ~/.bash_history 
+# places it in ~/history_backups/ directory
+# names it after the date that the commands were executed on
+# in yymmdd format
+# also compares disk usage of / 
+
+pd=$(<$HOME/history_backups/naming)
+td=$(date +"%y%m%d")
+s1=$HOME/daily_scans/$pd
+s2=$HOME/daily_scans/$td
+
+if [ $pd != $td ]; then		
+	cp $HOME/.bash_history $HOME/history_backups/$pd	
+	truncate -s 0 $HOME/.bash_history
+
+	sudo du --exclude=/{proc,sys,dev,run,tmp,var/cache,nix,home/ss/daily_scans,home/ss/history_backups} -aS / > $s2
+	diff -y --suppress-common-lines --width=250 $s1 $s2 > $HOME/daily_scans/d_$pd
+	mv $s2 $s1
+
+	echo $td > $HOME/history_backups/naming
+fi
