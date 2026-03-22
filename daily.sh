d5da3e841dd0f5d3ac030d61f08814c5dec4b9e5
Sharifjon Sharipov <ss2393506@gmail.com>
2026-03-22 16:18:33 +0000

    remove ls related lines; exclude librewolf config directory

--- daily.sh
+++ daily.sh
@@ -13,11 +13,13 @@ s2=$HOME/daily_scans/$td
 
 if [ $pd != $td ]; then		
 	cp $HOME/.bash_history $HOME/history_backups/$pd	
+	sed -i '/^ls\($\| \)/{ /|/!d }' $HOME/history_backups/$pd
 	truncate -s 0 $HOME/.bash_history
 
-	sudo du --exclude=/{proc,sys,dev,run,tmp,var/cache,nix,home/ss/daily_scans,home/ss/history_backups,home/ss/.cache} -aS / > $s2
+	sudo du --exclude=/{var,proc,sys,dev,run,tmp,nix,home/ss/daily_scans,home/ss/history_backups,home/ss/.cache,home/ss/.config/librewolf} -aS / > $s2
 	diff -u $s1 $s2 > $HOME/daily_scans/d_$pd
 	rm -rf $s1
 
 	echo $td > $HOME/history_backups/naming
+
 fi
