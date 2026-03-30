47b8a116da56b88c013c85dc88f37e915a9a7f9f
Sharifjon Sharipov <ss2393506@gmail.com>
2026-03-30 14:52:44 +0000

    more filter for bash history with external file

--- daily.sh
+++ daily.sh
@@ -12,14 +12,12 @@ s1=$HOME/daily_scans/$pd
 s2=$HOME/daily_scans/$td
 
 if [ $pd != $td ]; then		
-	cp $HOME/.bash_history $HOME/history_backups/$pd	
-	sed -i '/^ls\($\| \)/{ /|/!d }' $HOME/history_backups/$pd
+	sed -E -f <(awk '{print "/^" $0 "($| )/{ /[|>&]/!d }"}' $HOME/.local/bin/filters) $HOME/.bash_history > $HOME/history_backups/$pd
 	truncate -s 0 $HOME/.bash_history
-
-	sudo du --exclude=/{var,proc,sys,dev,run,tmp,nix,home/ss/daily_scans,home/ss/history_backups,home/ss/.cache,home/ss/.config/librewolf} -aS / > $s2
+	sudo du --exclude=/{var,proc,sys,dev,run,tmp,home/ss/daily_scans,home/ss/history_backups,home/ss/.cache,home/ss/.config/librewolf} -aS / > $s2
 	diff -u $s1 $s2 > $HOME/daily_scans/d_$pd
 	rm -rf $s1
 
 	echo $td > $HOME/history_backups/naming
-
+	grep "^[+-]" $HOME/daily_scans/d_$pd | tail -n +3 | awk '{sum += $1} END {print sum}'
 fi
